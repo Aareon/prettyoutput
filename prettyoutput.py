48879fd10ecda824e34f95796ac685ff963ee44a
Aareon <askully13@gmail.com>
2017-04-03 02:59:52 +0000

    Correct naming conventions

--- prettyoutput.py
+++ prettyoutput.py
@@ -7,7 +7,6 @@ Pretty Output
 Aareon Sullivan - 2017
 """
 
-
 color = {'red': '\033[0;31;40m',
          'yellow': '\033[1;33;40m',
          'green': '\033[1;32;40m',
@@ -18,8 +17,7 @@ color = {'red': '\033[0;31;40m',
 
 reset = color.get('reset')
 
-
-def status(string, color_code, stat_msg, prn_out, time, space):
+def _status(string, color_code, stat_msg, prn_out, time, space):
     message = _format(color_code, stat_msg, string, space)
     if time:
         stat_msg += '[' + strftime("%Y-%m-%d/%H:%M:%S", gmtime()) + ']'
@@ -27,7 +25,6 @@ def status(string, color_code, stat_msg, prn_out, time, space):
     if prn_out:
         print(message)
     return message
-
 
 def _format(color_code, stat_msg, string, space):
     color_code = color.get(color_code)
@@ -41,16 +38,14 @@ def _format(color_code, stat_msg, string, space):
         i = 10
     return color_code + stat_msg.ljust(i) + '| ' + reset + string
 
-error = partial(status, string='An error has ocurred!', color_code='red', stat_msg='[ERROR]', prn_out=True, time=False, space=False)
-warning = partial(status, string='Something is not right', color_code='yellow', stat_msg='[WARNING]', prn_out=True, time=False, space=False)
-success = partial(status, string='Great success!', color_code='green', stat_msg='[SUCCESS]', prn_out=True, time=False, space=False)
-info = partial(status, string='Information:', color_code='cyan', stat_msg='[INFO]', prn_out=True, time=False, space=False)
-custom = partial(status, string='Custom text', color_code='magenta', stat_msg='[CUSTOM]', prn_out=True, time=False, space=False)
-
+error = partial(_status, string='An error has ocurred!', color_code='red', stat_msg='[ERROR]', prn_out=True, time=False, space=False)
+warning = partial(_status, string='Something is not right', color_code='yellow', stat_msg='[WARNING]', prn_out=True, time=False, space=False)
+success = partial(_status, string='Great success!', color_code='green', stat_msg='[SUCCESS]', prn_out=True, time=False, space=False)
+info = partial(_status, string='Information:', color_code='cyan', stat_msg='[INFO]', prn_out=True, time=False, space=False)
+custom = partial(_status, string='Custom text', color_code='magenta', stat_msg='[CUSTOM]', prn_out=True, time=False, space=False)
 
 def color_this(string, color_code):
     return color.get(color_code)+string+reset
-
 
 def extend(tup, color_code='yellow', extens='>>>', prn_out=True):
     """Takes a tuple, returns a sub-message"""
@@ -61,7 +56,6 @@ def extend(tup, color_code='yellow', extens='>>>', prn_out=True):
     if prn_out:
         print(string)
 
-
 def version(color_code='magenta', stat_msg='[PRTTYOUT]', string='Pretty Output version info:', prn_out: bool=True):
     version = {'python': str(sys.version_info[0]) + '.' + str(sys.version_info[1]),
                'output': '2.8.4'}
@@ -69,6 +63,6 @@ def version(color_code='magenta', stat_msg='[PRTTYOUT]', string='Pretty Output v
     pyver = color_this(version.get('python'), 'magenta')
     outver = color_this(version.get('output'), 'magenta')
 
-    status(string, color_code, stat_msg, prn_out)
+    _status(string, color_code, stat_msg, prn_out)
     extend(('Python Version - ' + pyver, 'PreOut Version - ' + outver))
 
